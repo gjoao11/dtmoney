a85107a16159df2321d2b4deb9b669eab84579b2
Joao Gilberto <joaogjoao11@hotmail.com>
2021-09-26 12:45:40 -0300

    Create transactions context and hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
+
+import { TransactionsProvider } from './hooks/useTransactions';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
-import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -20,7 +22,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -31,6 +33,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
